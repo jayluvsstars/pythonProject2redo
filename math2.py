2fa0baf516d4058708e4c92a35b51323641da63b
Jalynn Bolds <boldsjal000@warren.k12.in.us>
2024-08-23 08:22:19 -0400

    Process finshed with exit code 0

---
+++ math2.py
@@ -0,0 +1,7 @@
+miles = 5 #pretend we got from user
+minutes = 45 #pretend we got from user
+hours = minutes / 60
+moh = miles / hours
+print("how many miles did you run?", 5)
+print("How many mintutes did it take you?", 45)
+print("speed in mph", 60)
